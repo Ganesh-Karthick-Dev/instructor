2e0592dd439b6906ac7886f21c714651d9fe241c
Karthick <ganeshkarthik18697@gmail.com>
2024-05-14 18:33:08 +0530

    deploy build file

---
+++ build/static/js/762.78b6221c.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmantis_material_react=self.webpackChunkmantis_material_react||[]).push([[762],{4762:function(a){a.exports=JSON.parse('{"sample-page":"Sample Page","documentation":"Documentation","others":"Others","roadmap":"Roadmap"}')}}]);
